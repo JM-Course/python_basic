b57777f6777991fe96d65a502f3bbe60828a5441
JM-Course <jimin.course.@gmail.com>
2019-06-23 15:20:16 +0900

    1주차 숙제 추가

---
+++ week_1/hw/hw1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+student2score = {
+    "Darius": 100,
+    "Dr. Mundo": 80,
+    "Morgana": 60,
+    "Sivir": 75,
+    "Yummi": 20,
+    "Viktor": 97
+}
+
+
+def get_special_students(student2score):
+    """
+    특별반 학생의 리스트를 리턴하는 함수
+    특별반은 점수가 80점 이상이어야 들어갈 수 있다.
+
+    :param student2score:
+    :return special_students:
+    """
+    special_students = []
+    return special_students
